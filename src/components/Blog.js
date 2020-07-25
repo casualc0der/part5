fa8819caf1b0e36c50dd469f4312e6bbccac19ba
casualc0der <edwardsansome@gmail.com>
2020-07-25 07:09:13 +0100

    add visibility toggle to blogs

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,23 +1,27 @@
-import React from "react";
-import Togglable from "../components/Togglable";
+import React, { useState } from "react";
+
 const Blog = ({ blog }) => {
-  const blogStyle = {
-    padding: 5,
-    borderWidth: 1,
-    borderStyle: "solid",
-    borderColor: "green",
-  };
+  const [blogVisible, setBlogVisible] = useState(false);
+
+  const hideWhenVisible = { display: blogVisible ? "none" : "" };
+  const showWhenVisible = { display: blogVisible ? "" : "none" };
+
   return (
-    <div style={blogStyle}>
-      Title: {blog.title}
-      {/* <Togglable buttonLabel={"view"}> */}
-      <p>Author: {blog.author}</p>
-      <p>URL: {blog.url}</p>
-      <p>
-        Likes: {blog.likes}
-        <button>like</button>
-      </p>
-      {/* </Togglable> */}
+    <div>
+      <div style={hideWhenVisible}>
+        <p>Title: {blog.title}</p>
+        <button onClick={() => setBlogVisible(true)}>Show details</button>
+      </div>
+      <div style={showWhenVisible}>
+        <p>Title: {blog.title}</p>
+        <p>Author: {blog.author}</p>
+        <p>URL: {blog.url}</p>
+        <p>
+          Likes: {blog.likes}
+          <button>like</button>
+        </p>
+        <button onClick={() => setBlogVisible(false)}>Hide</button>
+      </div>
     </div>
   );
 };
